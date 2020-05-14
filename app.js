14a84977fe10ac64d93920f1b81aa25b544a1408
Hasan1284 <hasansevindik1284@gmail.com>
2020-05-14 18:53:20 +0200

    my first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// // // map uygulaması nedir
+
+// //  var numbers = [15, 24, 53, 8, 16, 2, 35];
+// // // var addedNumbers = numbers.map(function(number) {
+// // //   return number += 8;
+// // // });
+// // // console.log(addedNumbers);
+
+// // var addedNumbers2 = numbers.map((number) => { return number + 8; });
+// //  console.log(addedNumbers2);
+// // var addedNumbers2 = numbers.map(number => number + 8); console.log(addedNumbers2);
+// // yukarıdaki ifadede number tek parametre olduğu için de parantez olmadan da kullana bildik
+/* var numbers = [15, 24, 53, 8, 16, 2, 35];
+var addedNumbers = numbers.map(function(deneme) {
+      return deneme += 8;
+    });
+console.log(addedNumbers);
+ */
