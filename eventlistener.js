39b00982dc1d7f9dd9fcf372208b5ae09fa8b2e0
Hasan1284 <hasansevindik1284@gmail.com>
2020-05-19 11:24:18 +0200

    event bubling and capturing

--- eventlistener.js
+++ eventlistener.js
@@ -105,3 +105,65 @@
 //     e.preventDefault();
 
 // }
+
+// event bubling and Capturin uygulamaları
+
+const form = document.querySelector('form');
+const cardBoby = document.querySelector('.card-body');
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+// form.addEventListener('click',function(e){
+//     console.log('form');
+//     e.stopPropagation();
+// });
+
+// cardBoby.addEventListener('click',function(e){
+//     console.log('card body');
+//     e.stopPropagation();
+// });
+
+// card.addEventListener('click',function(e){
+//     console.log('card');
+//     e.stopPropagation();
+// });
+
+// container.addEventListener('click',function(e){
+//     console.log('container');
+//     e.stopPropagation();
+// });
+
+// form.addEventListener('click',function(e){
+//     console.log('form');
+//     e.stopPropagation();
+// },true);
+
+// cardBoby.addEventListener('click',function(e){
+//     console.log('card body');
+//     e.stopPropagation();
+// },true);
+
+// card.addEventListener('click',function(e){
+//     console.log('card');
+//     e.stopPropagation();
+// },true);
+
+// container.addEventListener('click',function(e){
+//     console.log('container');
+//     e.stopPropagation();
+// },true);
+
+// // const deleteItems = document.querySelectorAll('.fa-times');
+// // deleteItems.forEach(function(item){
+// //     item.addEventListener('click',function)
+// //     console.log(e.target);
+// // });
+
+// const ul = document.querySelector('ul');
+
+// ul.addEventListener('click',function(e){
+//     if(e.target.className==='fas fa-times'){
+//         e.target.parentElement.parentElement.remove();
+//         e.preventDefault();
+//     }
+// })
