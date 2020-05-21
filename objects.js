b8b138437e16d8955b86057309621d9e053dcc77
Hasan1284 <hasansevindik1284@gmail.com>
2020-05-21 11:26:46 +0200

    object literals

---
+++ objects.js
@@ -0,0 +1,67 @@
+// // //objeckts
+// // let val;
+
+// // let num = 10;
+// // val = num;
+
+// // object literals
+// // let yigit = {
+// //     name: 'yigit',
+// //     yearOfBirth: 2010,
+// //     job: 'stdudent'
+// // }
+// // val = yigit
+
+
+
+
+
+
+// // // array
+
+// //  let numbers = [10,20,30];
+
+// //  val = numbers;
+ 
+// //  console.log(val);
+// // console.log(typeof val);
+
+
+// // let yigit = {
+//     //     name: 'yigit',
+//     //     yearOfBirth: 2010,
+//     //     job: 'stdudent'
+//     // }
+    
+//     // console.log(yigit);
+    
+//     // function constructure
+
+//     // function Person(name,yearOfBirth,job){
+//     //     this.name = name;
+//     //     this.yearOfBirth = yearOfBirth;
+//     //     this.job = job;
+//     //     this.calculateAge = function(){
+//     //         return 2018 - this.yearOfBirth;
+//     //     }
+
+//     //     // console.log(this);
+//     // }
+
+//     // asağıdaki ile yukarıda ki yazım tarzı da aynı işi görüyor
+
+//     let Person = function(name,yearOfBirth,job){
+//         this.name = name;
+//         this.yearOfBirth = yearOfBirth;
+//         this.job = job;
+//         this.calculateAge = function(){
+//             return 2018 - this.yearOfBirth;
+//     }
+
+//     let yigit = new Person('yigit',2010,'student');
+//     let emel = new Person('emel',1980,'teacher');
+
+//     console.log(yigit.name);
+//     console.log(yigit.yearOfBirth);
+//     console.log(yigit.job);
+//     console.log(yigit.calculateAge());
