92d2c4802d72e570a0c3acbd6ffaa3de0215ca5f
Hasan1284 <hasansevindik1284@gmail.com>
2020-06-10 08:38:13 +0200

    calback functions

--- constructor.js
+++ constructor.js
@@ -174,3 +174,29 @@
 // }
 
 // filterProducts(products);
+
+// callback fonksiyonlar
+
+function MultipleByTwo(a,b,c,
+    callback){
+    let arr = [];
+    for(let i=0,i<3,i++){
+        arr[i] = callback(arguments[i]*2);
+    }
+    return arr;
+}
+
+function addOne(a){
+    return a+1;
+}
+
+
+val = MultipleByTwo(5,10,20);
+
+// val = addOne(10);
+
+for(let i=0;i<val.length;i++){
+    val[i] = addOne(val[i]);
+}
+
+console.log(val);
