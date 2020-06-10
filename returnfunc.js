b527a911120dab4038d34a1d61cb38ceb8d9e406
Hasan1284 <hasansevindik1284@gmail.com>
2020-06-10 11:56:38 +0200

    apply call bind metotları

--- returnfunc.js
+++ returnfunc.js
@@ -76,28 +76,71 @@
 //     }
 // }
 
-Object.defineProperty(person, 'fullName', {
+// Object.defineProperty(person, 'fullName', {
     
-    get function(){
-        return `${this.firstName}
-         ${this.lastName}`
-    },
-    set function(value){
-        const parts = value.split(' ');
-        this.firstName = parts[0];
-        this.lastName = parts[1];
+//     get function(){
+//         return `${this.firstName}
+//          ${this.lastName}`
+//     },
+//     set function(value){
+//         const parts = value.split(' ');
+//         this.firstName = parts[0];
+//         this.lastName = parts[1];
+//     }
+// }
+// Object.defineProperty(person, 'age', {
+//     value : 50,
+
+//     writable : false
+
+//     })
+
+
+// person.Fullname = 'elif aslı'
+// console.log(person.fullName);
+// console.log(person);
+// // person.setFullname('elif aslı');
+// // console.log(person.fullName());
+
+// call apply and bind
+
+var welcome = function(a,b){
+    console.log('welcome' +this.name+ '.Areyou interested in'+a+ ' and'+b);
+}
+
+var yigit = {name:'Yiğit'};
+var ada = {name : 'Ada'};
+
+welcome.call(yigit,'asp.net','angular');
+welcome.call(ada);
+
+welcome.apply(yigit,['asp.net','angular']);
+welcome.apply(ada);
+
+welcomeYigit = welcome.bind(yigit);
+welcomeYigit();
+
+// call apply bind ile ilgili örnek
+
+var num = {
+    min : 0,
+    max : 100,
+    checkNumericRange : function(value){
+        if(typeof value !== 'number')
+            return false;
+    }else{
+        return value>=this.min && value<max;
     }
 }
-Object.defineProperty(person, 'age', {
-    value : 50,
 
-    writable : false
+console.log(num.checkNumericRange(20));
+console.log(num.checkNumericRange(-20));
 
-    })
+var num1 = {min : 50, max : 60};
+console.log(num.checkNumericRange.call(num1,65));
+console.log(num.checkNumericRange.apply(num1,[52]));
+var checkNumer = num.checkNumericRange.bin(num1);
 
+console.log(checkNumer(56));
 
-person.Fullname = 'elif aslı'
-console.log(person.fullName);
-console.log(person);
-// person.setFullname('elif aslı');
-// console.log(person.fullName());
+    
